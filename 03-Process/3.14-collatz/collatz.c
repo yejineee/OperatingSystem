6030e012b1f66ff16a3503eff6ff0d0c8a469d77
yejineee <yygeniee@gmail.com>
2021-04-17 15:58:05 +0900

    feat: 콜라츠 추측을 적용한 숫자의 sequence 출력 함수 구현
    파라미터로 숫자를 받아서 1이 될 때까지의 과정을 프린트해주는 함수를
    구현하였다.
    해당 함수를 헤더파일로 추가하였다.

---
+++ 03-Process/3.14-collatz/collatz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+long long collatz(long long num){
+  return num % 2 == 0 ? num / 2 :  num * 3 + 1;
+}
+
+void printCollatzSequence(long long num){
+	printf("Collatz Sequence : %lld\t", num);
+	
+	while(num != 1){
+		num = collatz(num);
+		printf("%lld\t", num);
+	}
+	puts("");
+}	
