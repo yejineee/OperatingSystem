d0735022e962f457c0371b57dccd6a3a4e2b8f2b
yejineee <yygeniee@gmail.com>
2021-04-20 02:58:38 +0900

    feat: shared memory로 collatz sequence 쓰기 구현
    shared memory를 구성한 후, 자식 프로세스가 shared memory에
    콜라츠 순서를 적은 후, 부모 프로세스가 그걸 읽어오는 프로그램을 구현하였다.

--- 03-Process/3.15-collatz_shard-memory/main.c
+++ 03-Process/3.15-collatz_shard-memory/main.c
@@ -5,22 +5,73 @@
 #include <ctype.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
+//shared memory
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
 #include "error.h"
 #include "collatz.h"
 
-bool isPositiveInt(int num){
-	return isdigit(num) == 0 && num > 0; 
+const int ERROR = -1;
+const char* NAME = "collatz";
+const int SIZE = 4096;
+
+bool isPositiveInt(char* num){
+	int len = strlen(num);
+	for(int i = 0; i < len; i++){
+		if(isdigit(num[i]) == false){
+			return false;
+		}
+	}
+	return atoi(num) > 0; 
 }
 
 bool isErrorInInput(int argc, char* argv[]){
 	if(argc != 2){
-		printf("%s", inputErrorMsg);
+		fprintf(stderr, "%s", inputErrorMsg);
 		return true;
-	}else if(!isPositiveInt(atoi(argv[1]))){
-		printf("%s", inputRangeErrorMsg);
+	}else if(!isPositiveInt(argv[1])){
+		fprintf(stderr, "%s", inputRangeErrorMsg);
 		return true;
 	}
 	return false;
+}
+
+void* createSharedMemory(const char* name, int size){
+	mode_t rwMode = 0666;
+	int shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666);
+	if(shm_fd == ERROR){
+		fprintf(stderr, "parent - %s - errno: %s \n", shmOpenErrorMsg, strerror(errno));
+		return NULL;
+	}
+	int code = ftruncate(shm_fd, size);
+	if(code == ERROR){
+		fprintf(stderr, "parent - %s - errno: %d, %s \n", ftruncateErrorMsg, errno, strerror(errno));
+		return NULL;
+	}
+	void* ptr = mmap(0, size, PROT_READ, MAP_SHARED, shm_fd, 0);
+	if(ptr == MAP_FAILED){
+		fprintf(stderr, "parent - %s - errno: %s \n", mmapErrorMsg, strerror(errno));
+		return NULL;
+	}
+	return ptr;
+}
+
+void* openSharedMemory(const char* name, int size){
+	mode_t rwMode = 0666;
+	int shm_fd = shm_open(name, O_RDWR, rwMode);
+	if(shm_fd == ERROR){
+		fprintf(stderr, "%s - errno: %s \n", shmOpenErrorMsg, strerror(errno));
+		return NULL;
+	}
+	void* ptr = mmap(0, size, PROT_WRITE, MAP_SHARED, shm_fd, 0);
+	if(ptr == MAP_FAILED){
+		fprintf(stderr, "%s - errno: %s \n", mmapErrorMsg, strerror(errno));
+		return NULL;
+	}
+	return ptr;
 }
 
 int main (int argc, char* argv[])
@@ -30,25 +81,31 @@ int main (int argc, char* argv[])
 	};
 	int startNum = atoi(argv[1]);
 	pid_t childPid ;
-	
-	/*
-		 shared-memory object 생성하기
-		 1. shm_open
-		 2. ftruncate
-		 3. mmap
-	*/
+
+	void* mmapPtr = createSharedMemory(NAME, SIZE);
+	if(mmapPtr == NULL){
+		return 1;
+	}
 
 	childPid = fork() ;
 
 	if (childPid == 0) { // child process
-		printCollatzSequence(startNum);
+		void* mmapPtr = openSharedMemory(NAME, SIZE);
+		long long num = startNum;
+		char buf[SIZE];
+
+		int n = snprintf(buf, SIZE, "Collatz Sequence : %lld\t", num);
+		while(num != 1){
+			num = collatz(num);
+			n += snprintf(buf+n, SIZE-strlen(buf), "%lld\t", num);
+		}
+		snprintf(buf+n, SIZE-strlen(buf), "\n");
+		snprintf(mmapPtr, SIZE, "%s", buf);
 	} 
 	else if (childPid > 0) { // parent process
 		wait(0x0);
-		/*
-		 1. print out contents of shared memory
-		 2. remove shared memory object
-		*/
+		printf("%s", (char*)mmapPtr);
+		shm_unlink(NAME);
 	}
 	else /* child_pid < 0 */ {
 		fprintf(stderr, "Fork failed.\n") ;
